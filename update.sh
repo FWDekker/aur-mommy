d0df3bcaa879c8b754ccbe0d89c026bb45d87870
Florine W. Dekker <florine@fwdekker.com>
2023-11-29 17:40:32 +0100

    🏹 mommy can now point to individual commits~

--- update.sh
+++ update.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+
+# Usage:
+# `./update.sh v1.2.3` points the build instructions to the tag `v1.2.3`
+# `./update.sh 0c72a50` points the build instructions to the commit `0c72a50`
+#
+# The difference is detected based on whether the target starts with `v`.
+
 set -e
 cd -P -- "$(dirname -- "$0")"
 
 VERSION="${1:?Version not specified}"
 
-if [ "$VERSION" = "dev" ]; then
-    VERSION="dev"
-    URL="git+https://github.com/FWDekker/mommy.git#branch=dev"
-    REL="$(grep "pkgrel=" PKGBUILD)"
-    REL="${REL##*=}"
-else
+if [ "${VERSION::1}" = "v" ]; then
     VERSION="${VERSION##v}"
     URL="git+https://github.com/FWDekker/mommy.git#tag=v\$pkgver"
     REL="1"
+else
+    URL="git+https://github.com/FWDekker/mommy.git#commit=$VERSION"
+    REL="$(grep "pkgrel=" PKGBUILD)"
+    REL="${REL##*=}"
 fi
 
 sed -i -E "s|pkgver=.*|pkgver=$VERSION|; s|pkgrel=.*|pkgrel=$REL|; s|source=.*|source=(\"$URL\")|;" PKGBUILD
